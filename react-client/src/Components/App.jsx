7b4b3ccf6c09df4b3b1f902dbb45fc6eca4a8d18
yaminiPonugoti <yponugot@purdue.edu>
2019-12-30 15:20:43 +0700

    trying to get basic react and node set up

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomePage from './HomePage';
+import Footer from './FooterComponent/Footer'
 import NavBar from './HeaderComponent/NavBar';
 class App extends Component {
   render() {
